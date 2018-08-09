144948b9787a7a7b6a0d9e3854cdca323a4153ee
Michael Fienen <fienen@gmail.com>
2018-08-09 16:16:59 -0500

    Resolves #1: Show taxonomy in REST for Gutenberg

--- includes/class-ssp-speakers.php
+++ includes/class-ssp-speakers.php
@@ -181,19 +181,22 @@ class SSP_Speakers {
 
 		// Build taxonomy arguments
         $args = array(
-        	'label' => $this->plural,
-        	'labels' => apply_filters( 'ssp_speakers_taxonomy_labels', $labels ),
-        	'hierarchical' => true,
-            'public' => true,
-            'show_ui' => true,
-            'show_in_nav_menus' => true,
-            'show_tagcloud' => true,
-            'meta_box_cb' => null,
-            'show_admin_column' => true,
-            'update_count_callback' => '',
-            'query_var' => $this->tax,
-            'rewrite' => array( 'slug' => apply_filters( 'ssp_speakers_taxonomy_slug', $this->tax ) ),
-            'sort' => '',
+					'hierarchical' 					=> true,
+					'label' 								=> $this->plural,
+					'labels' 								=> apply_filters( 'ssp_speakers_taxonomy_labels', $labels ),
+					'meta_box_cb' 					=> null,
+					'public' 								=> true,
+					'query_var' 						=> $this->tax,
+					'rewrite' 							=> array( 
+						'slug' 								=> apply_filters( 'ssp_speakers_taxonomy_slug', $this->tax ) 
+					),
+					'show_admin_column'			=> true,
+					'show_in_nav_menus'			=> true,
+					'show_in_rest' 					=> true,
+					'show_tagcloud'					=> true,
+					'show_ui' 							=> true,
+					'sort' 									=> '',
+					'update_count_callback' => '',
         );
 
         // Allow filtering of taxonomy arguments
